ca30ccd107c264ad91502327bfbaf5d78cbe555c
cl <761171478@qq.com>
2021-05-24 19:10:42 +0800

    修改bml后的的代码

--- js/index.js
+++ js/index.js
@@ -1,51 +1,51 @@
-let height = document.getElementById('data')
-let weight = document.getElementById('data2')
-let bml = document.getElementById('BMI') //bml --弹窗
-let bmi_span = document.getElementById('bmi-span') //BMI的span
-let table_arrow = document.getElementById('table_arrow') //表格箭头
-let btn = document.querySelector('.content .form .wrap-button button'); //点击按钮
-let x
-let digit
-let pop_up = document.querySelector('.pop-up'); //错误的时候的提示框
+let height = $('#data')
+let weight = $('#data2')
+let bmi_span = $('#bmi-span') //BMI的span
+let table_arrow = $('#table_arrow') //表格箭头
+let btn = $('.content .form .wrap-button button'); //点击按钮
+let bml = $('#BMI') //bml --弹窗
+
 function  showPopUp(msg ,timeout = 1500) {
+    let div = document.createElement('div')
+    div.classList.add('pop-up')
+    document.body.append(div)
+    let pop_up = $('.pop-up'); //错误的时候的提示框
     pop_up.style.display = 'block'
     pop_up.innerHTML = msg
     setTimeout(function (){
         pop_up.style.display = 'none'
     },timeout)
 }
+// 进行计算的方法
 function calc(){
-        x = weight.value / (height.value ** 2) * 10000; /*公式（BMI）=体重（kg）÷身高/2（m）*/
-        digit = x.toFixed(1) /*结果保留一个小数点*/ //digit-数字
+        //结果保留一个小数点
+        result = (weight.value / (height.value ** 2) * 10000).toFixed(1); /*公式（BMI）=体重（kg）÷身高/2（m）*/
+        function valuee(num,scope) {
+            bmi_span.innerText = '你的 BMI 值:' + result + ',身体状态：'+ scope + ' '
+            table_arrow.style.top = num + '%'
+        }
+        valuee(36,'偏瘦')
         if(!height.value || !weight.value) {
          return  showPopUp('不能是空的',2000)
         }else if(isNaN(height.value) || isNaN(weight.value)) {
-         return  showPopUp('不是数字',3000)
+         return  showPopUp('不是数字',2500)
         }else {
-                if (digit <= 18.4) {
-                    bmi_span.innerText = '你的 BMI 值:' + digit + ',身体状态：偏瘦' + ' '
-                    table_arrow.style.top = 36 + '%'
-                } else if (digit >= 18.5 && digit <= 23.9) {
-                    bmi_span.innerText = '你的 BMI 值:' + digit + ',身体状态：正常' + ' '
-                    table_arrow.style.top = 52 + '%'
-                } else if (digit >= 24.0 && digit <= 27.9) {
-                    bmi_span.innerText = '你的 BMI 值:' + digit + ',身体状态：过重' + ' '
-                    table_arrow.style.top = 71 + '%'
-                } else if (digit >= 28) {
-                    bmi_span.innerText = '你的 BMI 值:' + digit + ',身体状态：肥胖' + ' '
-                    table_arrow.style.top = 86 + '%'
+                if (result <= 18.4) {
+                    valuee(36,'偏瘦')
+                } else if (result >= 18.5 && result <= 23.9) {
+                    valuee(52,'正常')
+                } else if (result >= 24.0 && result <= 27.9) {
+                    valuee(71,'过重')
+                } else if (result >= 28) {
+                    valuee(86,'肥胖')
                 }
                 bml.style.display = 'block'
                 table_arrow.style.display = 'block' //表格箭头
-
-                // weight.value = ''
-                // height.value = ''
             table() //表格
         }
 }
-btn.onclick = function () {
-        calc() //计算
-}
+//点击按钮执行的事件
+btn.onclick = calc
 //键盘事件 --回车按钮
 weight.addEventListener('keypress',function (event){
         if (event.keyCode === 13) {
@@ -68,9 +68,9 @@ function down_timer(now = null) {
     sed = sed < 10 ? `0${sed}`:sed
     now = `${y}年${m}月${d}日${h}:${minute}:${sed}`
     return now
-}down_timer()
+}
 setInterval(function (){
-    return down_timer()
+   // console.log(down_timer())
 },1000)
 
 let count = 0 //数字累加
@@ -81,7 +81,7 @@ function table() {
     record.style.display = 'none' //记录消失
     count++
     //存储我的数据 增加一个数组
-    let data = [ count, down_timer(), height.value, weight.value, digit,''] //最后一个数据给为空的
+    let data = [ count, down_timer(), height.value, weight.value, result,''] //最后一个数据给为空的
     // 第二种办法 插入html
     let tableS = document.createElement('table');
     empty.append(tableS)
@@ -98,25 +98,7 @@ function table() {
     a.setAttribute('href',"javascript:void(0)")
     a.innerHTML = '删除'
 
-
-
-    //第一种办法 拼接html
-    // empty.innerHTML +=
-    //     ` <table>
-    //          <tr>
-    //              <td>${count}</td>
-    //              <td>${down_timer()}</td>
-    //              <td>${height.value}</td>
-    //              <td>${weight.value}</td>
-    //              <td>${digit}</td>
-    //              <td>
-    //                 <a href="javascript:void(0)">删除</a>
-    //              </td>
-    //          </tr>
-    //      </table>`
-
     //删除
-
     let del = document.querySelectorAll('footer .wrap-record .empty table tr td a')
     for (let i = 0; i < del.length; i++) {
         del[i].onclick = function () {
@@ -124,7 +106,6 @@ function table() {
            // let tb = this.parentNode.parentNode.parentNode.parentNode
            // let tb = del[i].closest('table')
             let chuli = confirm('请问是否要删除呢?')
-            debugger
             if (chuli) {
                 let tb = this.closest('table')
                 tb.remove()
@@ -132,7 +113,22 @@ function table() {
 
         }
     }
-
-
 }
 
+
+
+
+//第一种办法 拼接html
+// empty.innerHTML +=
+//     ` <table>
+//          <tr>
+//              <td>${count}</td>
+//              <td>${down_timer()}</td>
+//              <td>${height.value}</td>
+//              <td>${weight.value}</td>
+//              <td>${result}</td>
+//              <td>
+//                 <a href="javascript:void(0)">删除</a>
+//              </td>
+//          </tr>
+//      </table>`
